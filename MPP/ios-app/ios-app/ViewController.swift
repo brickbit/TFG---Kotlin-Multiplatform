071cf928e526b23bf463d9abc8b8d47128c53165
Roberto <92rober@gmail.com>
2020-07-19 21:03:37 +0200

    added iOS configuration

--- MPP/ios-app/ios-app/ViewController.swift
+++ MPP/ios-app/ios-app/ViewController.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
+import MultiPlatformLibrary
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  override func viewDidLoad() {
+    // ...
+    HelloWorld().print()
+    IosHelloWorld().print()
+  }
 }
 
