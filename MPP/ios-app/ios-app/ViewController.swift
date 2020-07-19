8b69cebf999471ba5e94019f1bcc0d7af07a8c8c
Roberto <92rober@gmail.com>
2020-07-19 20:57:38 +0200

    added new iOS project

---
+++ MPP/ios-app/ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-app
+//
+//  Created by Roberto García Romero on 19/07/2020.
+//  Copyright © 2020 Roberto García Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
