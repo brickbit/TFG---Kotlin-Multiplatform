446a4175523971c0ab3254ba1e980b207c846ac7
Roberto <92rober@gmail.com>
2020-07-25 23:18:34 +0200

    Added Base Presenter

--- MPP/mpp-library/src/commonMain/kotlin/domain/presenter/BasePresenter.kt
+++ MPP/mpp-library/src/commonMain/kotlin/domain/presenter/BasePresenter.kt
@@ -1,6 +1,46 @@
+import domain.error.Either
+import domain.error.ErrorHandler
+import domain.error.Result.CustomError
+import domain.executor.Executor
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.withContext
 
-abstract class BasePresenter<out V : BasePresenter.View>() {
+abstract class BasePresenter<out V : BasePresenter.View>(
+    protected val errorHandler: ErrorHandler,
+    protected val executor: Executor,
+    val view: V
+) {
+
+    private val job = SupervisorJob()
+
+    protected val scope = CoroutineScope(job + executor.main)
+
+    abstract fun attach()
+
+    fun detach() = job.cancel()
+
+    protected suspend fun <T> execute(f: suspend () -> Either<CustomError, T>): Either<CustomError, T> = withContext(executor.background) { f() }
+
+    protected fun onError(callback: (String) -> Unit): (CustomError) -> Unit = {
+        view.hideProgress()
+
+        val message = errorHandler.convert(it)
+
+        callback(message)
+    }
 
     interface View {
+        fun showProgress()
+
+        fun hideProgress()
+
+        fun showError(error: String)
+
+        fun showError(errorId: Int)
+
+        fun showMessage(message: String)
+
+        fun showMessage(messageId: Int)
     }
 }
